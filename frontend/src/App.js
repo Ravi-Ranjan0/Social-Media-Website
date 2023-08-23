820d8cf12f7a2a349a3b7dc714d6de5e1a454d0b
Falgun Patel <falgunmpatel9123@gmail.com>
2023-08-23 14:31:19 +0530

    front-end updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
+import "./App.css";
+import Friends from "./components/Friends";
+import Group from "./components/Group";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h1>Front End</h1>
+    <>
+      <div className="bg-slate-300">
+        <Navbar />
+        <Group />
+        <Friends />
+      </div>
+    </>
   );
 }
 
